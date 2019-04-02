294aca8c946593385339dbad1b5f17eef2c14597
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-02 14:37:19 -0500

    finished mergesort

--- mergesort.js
+++ mergesort.js
@@ -1,27 +1,45 @@
 function split(array) {
-    let splitIdx = Math.floor(array.length/2)
-    let firstHalf = array.slice(0, splitIdx)
-    let secondHalf = array.slice(splitIdx, array.length)
-    return [firstHalf, secondHalf]
+  let splitIdx = Math.floor(array.length / 2);
+  let firstHalf = array.slice(0, splitIdx);
+  let secondHalf = array.slice(splitIdx, array.length);
+  return [firstHalf, secondHalf];
 }
 
 function merge(arr1, arr2) {
-    let i = 0
-    let j = 0
-    let output = []
-    while(i < arr1.length && j < arr2.length) {
-        if (arr1[i] > arr2[j]) {
-            output.push(arr2[j])
-            j++
-        } else {
-            output.push(arr1[i])
-            i++
-        }
-    }
-    if (i > arr1.length - 1) {
-        output.push(...arr2.slice(j))
+  let i = 0;
+  let j = 0;
+  let output = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      output.push(arr2[j]);
+      j++;
     } else {
-        output.push(...arr1.slice(i))
+      output.push(arr1[i]);
+      i++;
     }
-    return output
+  }
+  if (i > arr1.length - 1) {
+    output.push(...arr2.slice(j));
+  } else {
+    output.push(...arr1.slice(i));
+  }
+  // while (i < arr1.length) {
+  //     output.push(arr1[i]);
+  //     i++;
+  // }
+  // while (j < arr2.length) {
+  //     output.push(arr2[j]);
+  //     j++;
+  // }
+  return output;
+}
+
+function mergeSort(array) {
+  if (array.length === 1) return array;
+  let [left, right] = split(array);
+
+  let sortedLeft = mergeSort(left);
+  let sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,17 +1,34 @@
 describe('Split Array function', function() {
-    it('is able to split an even array into two halves', function() {
-        expect(split([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3], [4, 5, 6]])
-    });
-    it('is able to split an odd array into two halves', function() {
-        expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4, 5]])
-    })
+  it('is able to split an even array into two halves', function() {
+    expect(split([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3], [4, 5, 6]]);
   });
+  it('is able to split an odd array into two halves', function() {
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4, 5]]);
+  });
+});
 
-  describe('Merge Function', function() {
-      it('is able to merge two arrays of even length', function() {
-          expect(merge([1, 3, 6], [2, 5, 7])).toEqual([1, 2, 3, 5, 6, 7])
-      });
-      it('is able to merge two arrays of uneven lengths', function() {
-          expect(merge([1, 3, 6, 7], [2, 4, 8])).toEqual([1, 2, 3, 4, 6, 7, 8])
-      })
-  })
+describe('Merge Function', function() {
+  it('is able to merge two arrays of even length', function() {
+    expect(merge([1, 3, 6], [2, 5, 7])).toEqual([1, 2, 3, 5, 6, 7]);
+  });
+  it('is able to merge two arrays of uneven lengths', function() {
+    expect(merge([1, 3, 6, 7], [2, 4, 8])).toEqual([1, 2, 3, 4, 6, 7, 8]);
+  });
+});
+
+describe('Merge Sort Function', function() {
+  it('is able to sort an array', function() {
+    expect(mergeSort([2, 1, 9, 7, 5, 4, 3, 6, 8, 0])).toEqual([
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ]);
+  });
+});
