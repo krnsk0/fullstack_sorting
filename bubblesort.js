c646936c3891aabaf8109a089c4cdaae8b391cd3
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-02 11:19:24 -0500

    added comparator function input

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,18 @@
-function bubbleSort (array) {
+function swap(x, y, array) {
+  [array[x], array[y]] = [array[y], array[x]];
+}
+
+function defaultComparator(a, b) {
+  return a > b;
+}
+
+function bubbleSort(array, comparator = defaultComparator) {
   for (let i = array.length - 1; i >= 0; i -= 1) {
     for (let j = 0; j < i; j += 1) {
-      if (array[j] > array[j + 1]) {
+      if (comparator(array[j], array[j + 1])) {
         swap(j, j + 1, array);
       }
     }
   }
   return array;
-};
-
-function swap (x, y, array) {
-  [array[x], array[y]] = [array[y], array[x]];
-};
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,14 +1,13 @@
 describe('Bubble Sort', function() {
-
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([]);
   });
 
   it('sorts two elements requires exactly one swap', function() {
-    console.log(swap)
+    console.log(swap);
 
     spyOn(window, 'swap').and.callThrough();
-    expect(bubbleSort([2,1])).toEqual([1,2])
+    expect(bubbleSort([2, 1])).toEqual([1, 2]);
     expect(swap.calls.count()).toEqual(1);
   });
 
@@ -27,6 +26,20 @@ describe('Bubble Sort', function() {
       8,
       9
     ]);
-    expect(swap.calls.count()).toEqual(23)
+    expect(swap.calls.count()).toEqual(23);
+  });
+
+  it('sorts objects by age', function() {
+    let arr = [{ age: 4 }, { age: 8 }, { age: 2 }, { age: 9 }];
+    function comparator(a, b) {
+      return a.age > b.age;
+    }
+
+    expect(bubbleSort(arr, comparator)).toEqual([
+      { age: 2 },
+      { age: 4 },
+      { age: 8 },
+      { age: 9 }
+    ]);
   });
 });
